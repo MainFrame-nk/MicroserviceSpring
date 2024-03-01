0aba963d47c3979334a8071f1cd7d0f458c07df2
MainFrame <nk.mframe@gmail.com>
2024-03-01 14:10:26 +0400

    Initial commit

---
+++ src/main/java/com/example/microservicespring/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.microservicespring.repository;
+
+import com.example.microservicespring.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByTitle(String title);
+}
